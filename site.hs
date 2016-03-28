495f02834b6fc0516fee2c45f6e4eaf42a4aa555
Chris Allen <cma@bitemyapp.com>
2016-03-28 17:40:23 -0500

    blah

--- site.hs
+++ site.hs
@@ -6,7 +6,7 @@ import Hakyll.Core.Configuration
 
 config :: Configuration
 config = defaultConfiguration
-        { deployCommand = "rsync -avz -e ssh ./_site/ bitemyapp.com:/var/www/bitemyapp.com/"}
+        { deployCommand = "rsync -avz -e ssh ./_site/ bitemyapp.com:/var/www/bitemyapp.com/" }
 
 feedConfig :: FeedConfiguration
 feedConfig = FeedConfiguration
